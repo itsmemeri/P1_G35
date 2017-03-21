ab2c460bdfd1587fffa0bd5b16d5a191df26c451
marialopezvivas <marialopezvivas@S1-12>
2017-03-21 13:51:30 +0100

    Add project files.

---
+++ CoinRace/Map.cpp
@@ -0,0 +1,14 @@
+#include "Map.h"
+
+enum class charmap { punto, arroba, dollar, MAX };
+int NUM_FILAS, NUM_COLUMNAS;
+charmap matriz[NUM_FILAS][NUM_COLUMNAS];
+
+Map::Map()
+{
+}
+
+
+Map::~Map()
+{
+}
---
+++ CoinRace/Source.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+
+int dificultad;
+int tamaño = 5 * dificultad + rand() % (10 * dificultad - 5 * dificultad);
