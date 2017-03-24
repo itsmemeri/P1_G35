6ecda84a8212d0ea5636692eac7222d99f3c46cd
Maria <marialopezvivas@gmail.com>
2017-03-25 00:06:29 +0100

    añadido print array

--- CoinRace/Main.cpp
+++ CoinRace/Main.cpp
@@ -20,8 +20,8 @@ class Map
 	char **map;
 	Map::Map(dificultad A)
 	{
-		int Rows = 5 * static_cast<int>(A) + rand() % (10 * static_cast<int>(A) - 5 * static_cast<int>(A));
-		int Columns = 5 * static_cast<int>(A) + rand() % (10 * static_cast<int>(A) - 5 * static_cast<int>(A));
+		Rows = 5 * static_cast<int>(A) + rand() % (10 * static_cast<int>(A) - 5 * static_cast<int>(A));
+		Columns = 5 * static_cast<int>(A) + rand() % (10 * static_cast<int>(A) - 5 * static_cast<int>(A));
 		matriz(Rows, Columns, map);
 		//Rellenar el array con puntos (mapa vacío)
 		for (int i = 0; i < Rows; i++)
@@ -36,6 +36,15 @@ class Map
 	void chichinabo(int x, int y, char Zelda)
 	{
 		map[x][y] = Zelda;
+	}
+	void printMap() {
+		for (int i = 0; i < Rows; i++)
+		{
+			for (int j = 0; j < Columns; j++) {
+				std::cout << map[i][j];
+			}
+			std::cout << std::endl;
+		}
 	}
 	Map::~Map()
 	{
