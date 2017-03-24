b5fce80856eacea9d6c4cb18ef2055a516af1a88
Maria <marialopezvivas@gmail.com>
2017-03-24 22:19:00 +0100

    Constructor matriz hecho

--- CoinRace/Map.cpp
+++ CoinRace/Map.cpp
@@ -1,14 +1,49 @@
 #include "Map.h"
 
 enum class charmap { punto, arroba, dollar, MAX };
-int NUM_FILAS, NUM_COLUMNAS;
-charmap matriz[NUM_FILAS][NUM_COLUMNAS];
+
 
 Map::Map()
 {
+	void matriz(int dificultad) 
+	{
+		int tamanio = 5 * dificultad + rand() % (10 * dificultad - 5 * dificultad);
+
+		//LEER el número de filas y columnas
+		int numRows = tamanio;
+		int numColumns = tamanio;
+
+		//PEDIR MEMORIA para un array de punteros
+		//Tantos punteros como filas
+		//Cada puntero apuntará a una columna
+		int **map = new int*[numRows];
+
+		//Para cada puntero fila, hay que pedir memoria para las columnas de esa fila
+		for (int i = 0; i < numRows; i++)
+		{
+			map[i] = new int[numColumns];
+		}
+
+		//ACCEDER con subíndices
+		for (int i = 0; i < numRows; i++)
+		{
+
+			for (int j = 0; j < numColumns; j++)
+			{
+				map[i][j] = 0;
+			}
+		}
+	}
 }
 
 
 Map::~Map()
 {
+	void BorrarMatriz()
+	{
+		for (int i = 0; i < numRows; i++) {
+			delete[]map[i];
+		}
+		delete[]map;
+	}
 }
--- Proban2/Source.cpp
+++ Proban2/Source.cpp
@@ -1,36 +1,37 @@
-#include <iostream>
+/*#include <iostream>
 //DECLARAR un puntero de puntero
-int **md;
-int numRows, numColumns;
-int dificultad;
-int tamanio = 5 * dificultad + rand() % (10 * dificultad - 5 * dificultad);
+void main() {
+	
+	int dificultad;
+	int tamanio = 5 * dificultad + rand() % (10 * dificultad - 5 * dificultad);
+	int numRows=tamanio, numColumns=tamanio;
+	//LEER el número de filas y columnas
+	numRows = tamanio;
+	numColumns = tamanio;
 
-//LEER el número de filas y columnas
-numRows = tamanio ;
-numColumns = tamanio;
+	//PEDIR MEMORIA para un array de punteros
+	//Tantos punteros como filas
+	//Cada puntero apuntará a una columna
+	int **md = new int*[numRows];
 
-//PEDIR MEMORIA para un array de punteros
-//Tantos punteros como filas
-//Cada puntero apuntará a una columna
-md = new int*[numRows];
-
-//Para cada puntero fila, hay que pedir memoria para las columnas de esa fila
-for (int i = 0; i < numRows; i++)
-{
-	md[i] = new int[numColumns];
-}
-
-//ACCEDER con subíndices
-for (int i = 0; i < numRows; i++)
-{
-
-	for (int j = 0; j < numColumns; j++)
+	//Para cada puntero fila, hay que pedir memoria para las columnas de esa fila
+	for (int i = 0; i < numRows; i++)
 	{
-		md[i][j] = 0;
+		md[i] = new int[numColumns];
 	}
-}
-//LIBERAR memoria
-for (int i = 0; i < numRows; i++) {
-	delete[]md[i];
-}
-delete[]md;
+
+	//ACCEDER con subíndices
+	for (int i = 0; i < numRows; i++)
+	{
+
+		for (int j = 0; j < numColumns; j++)
+		{
+			md[i][j] = 0;
+		}
+	}
+	//LIBERAR memoria
+	for (int i = 0; i < numRows; i++) {
+		delete[]md[i];
+	}
+	delete[]md;
+}*/
